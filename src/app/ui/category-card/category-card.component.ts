c4e74a621454a2c390b6d58fa4622c09dea05639
kanowory <kanowory@student.wszib.edu.pl>
2023-05-14 12:45:58 +0200

    Usuwanie kategorii

--- src/app/ui/category-card/category-card.component.ts
+++ src/app/ui/category-card/category-card.component.ts
@@ -1,15 +1,20 @@
-import {Component, Input} from '@angular/core';
-import {faComments, faPlay, faTrash} from "@fortawesome/free-solid-svg-icons";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { faComments, faPlay, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-category-card',
-  templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.css']
+  selector: "app-category-card",
+  templateUrl: "./category-card.component.html",
+  styleUrls: ["./category-card.component.css"],
 })
 export class CategoryCardComponent {
-  @Input() categoryName = '';
+  @Input() categoryName = "";
+  @Output() removeClicked = new EventEmitter<void>();
 
   trashIcon = faTrash;
   commentsIcon = faComments;
   playIcon = faPlay;
+
+  emitRemove() {
+    this.removeClicked.emit();
+  }
 }
